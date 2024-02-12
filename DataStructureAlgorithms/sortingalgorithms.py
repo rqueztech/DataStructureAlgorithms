ef91294de5af84117e51f7fd3482f977219d9a77
rqueztech <questadar41@gmail.com>
2024-02-12 11:59:36 +0900

    Set up the framework for an insertion sort class.

--- DataStructureAlgorithms/sortingalgorithms.py
+++ DataStructureAlgorithms/sortingalgorithms.py
@@ -1,4 +1,8 @@
 class SortingAlgorithms:
+    # set the class for the insertion sort
+    def insertionSort(arr):
+        print(arr)
+
     # set the class for the selection sort
     def selectionSort(arr):
         # create your outer loop which is going to iterate through array
@@ -9,7 +13,7 @@ class SortingAlgorithms:
             # iterate through every single element after the current minimum index
             for innerIndex in range(currentMinimumIndex + 1, len(arr) - 1):
                 # if the inner index is less than, capture it
-                if arr[currentMinimumIndex] < arr[innerIndex]:
+                if arr[currentMinimumIndex] > arr[innerIndex]:
                     currentMinimumIndex = innerIndex
 
             # once you are done capturing both indexes, do a swap
@@ -18,3 +22,5 @@ class SortingAlgorithms:
             arr[currentIndex] = swap
 
         print(arr)
+
+        print("\nExplanation: Time complexity of selection sort: o(n^2). Why is this? because you are iterating thorugh the array again within an iteration. when you do this, the time complexity is exponential because for every iteration. This is one of the worse time complexities, hence completely unideal for real world use, maybe limited cases.")
